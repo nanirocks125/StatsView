a8b95176bfe24b1d64a4d3ac3a1971bb21dc9782
nanirocks125 <nanirocks125@gmail.com>
2018-03-01 14:17:56 +0530

    Update StatsCardViewDelegate.swift

--- StatsCard/StatsCardViewDelegate.swift
+++ StatsCard/StatsCardViewDelegate.swift
@@ -1,10 +1,4 @@
-//
-//  StatsCardViewDelegate.swift
-//  StatsCard
-//
-//  Created by Pratian IOS on 28/02/18.
-//  Copyright © 2018 Pratian Technologies. All rights reserved.
-//
+
 
 import Foundation
 

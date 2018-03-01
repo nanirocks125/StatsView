e5c9a90afc2409e55eb15c20a2abb8546f4c9969
nanirocks125 <nanirocks125@gmail.com>
2018-03-01 14:16:45 +0530

    Update Card.swift

--- StatsCard/Card.swift
+++ StatsCard/Card.swift
@@ -1,10 +1,4 @@
-//
-//  Card.swift
-//  StatsCard
-//
-//  Created by Pratian IOS on 28/02/18.
-//  Copyright © 2018 Pratian Technologies. All rights reserved.
-//
+
 
 import Foundation
 public class Card {
